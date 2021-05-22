b03044b6c871e79ee8a775b400a524985ab39771
JaysonSovanHong <jaysonsovanhong@gmail.com>
2021-05-22 19:02:51 -0400

    stockupdate

--- application.py
+++ application.py
@@ -1,16 +1,29 @@
-
-import models
-from dotenv import load_dotenv
 import os
-from flask import Flask
+from flask.wrappers import Response
+import requests
+import sqlalchemy
+from werkzeug.datastructures import Authorization
+import models
+import jwt
+import bcrypt
+
+
+# from flask_cors import CORS
+# CORS(app)
+from dotenv import load_dotenv
+from flask import Flask, request
+
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 load_dotenv()
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+models.db.init_app(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-
-
-
+crypto_api = "https://api.coinranking.com/v2/coins?limit=100"
+api_key = os.environ.get("crypto_api")
 
 
 
@@ -18,8 +31,120 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 def root():
     return {"message":'ok'}
 
+@app.route('/user/login', methods=["POST"])
+def login():
+    user = models.User.query.filter_by(email = request.json['email']).first()
+    
+    if user.password == request.json['password']:
+        return{"user":user.to_json()}
+        
+    else:
+        return{'message':'login failed'},401
 
 
+
+@app.route('/user/signup',methods=["POST"])
+def sign_up():
+    try:
+        user = models.User(
+            name=request.json['name'],
+            email = request.json['email'],
+            password = request.json['password'],
+            balance = request.json['balance']
+        )
+        models.db.session.add(user)
+        models.db.session.commit()
+        return{"message":'welcome new user', "user": user.to_json()}
+    
+    except sqlalchemy.exc.IntegrityError:
+        return{"message":'can not signup'},401
+
+
+
+
+# @app.route('/user/verify',methods=["GET"])
+# def verify():
+#         user = models.User.query.filter_by(id=request.headers["Authorization"]).first()
+#         print(request.headers)
+#         if user:
+#             return{"user":user.to_json()}
+#         else:
+#             return{"message":'user not found'},404
+    
+@app.route('/user',methods=["GET", 'POST', 'DELETE'])
+def user_info():
+        
+    if request.method == 'GET':
+        try:
+            user = models.User.query.filter_by(id=request.headers["Authorization"]).first()
+            print(request.headers)
+            if user:
+                return {"user":user.to_json()}
+        except sqlalchemy.exc.IntegrityError:
+            return{'message':'can not find user'},404
+        
+    elif request.method == 'POST':
+        try:
+            user= models.User.query.filter_by(id=request.headers["Authorization"]).first()
+            if user:
+                user.name = request.json['name']
+                user.email = request.json['email']
+                user.password = request.json['password']
+                models.db.session.commit()
+                return {"user":user.to_json()}
+        except sqlalchemy.exc.IntegrityError:
+            return{'message':'can not update user'},404
+    # elif request.method == 'DELETE':
+    #     try:
+    #        user= models.User.query.filter_by(id=request.headers["Authorization"]).first()
+    #         if user:
+    #             models.db.session.delete(user)
+    #             models.db.session.commit()
+    #             return {"user":user.to_json()}
+    #     except sqlalchemy.exc.IntegrityError:
+    #         return{'message':'can not update user'},404
+
+             
+
+            
+    
+    
+    
+
+@app.route('/stocks',methods=["GET"])
+
+def stocks():
+    try:
+        response = requests.get(f'https://api.coinranking.com/v2/coins?limit=100').json()
+        if response:
+            print('found api',response)
+            return{"message": response}
+    except sqlalchemy.exc.IntegrityError:
+        return{'message':'can not find crypto'}
+        
+
+
+@app.route('/stock',methods=["GET"])
+def stock():
+    try:
+        
+        response = requests.get(f'https://api.coinranking.com/v2/search-suggestions?query={query}').json()
+        if response:
+            print('message', response)
+            return{'message':"one stock"}
+        
+    except sqlalchemy.exc.IntegrityError:
+        return{"message":'can not find stock'}
+        
+    
+
+@app.route('/stock/<int:id>/add',methods=["POST"])
+def add():
+    return{'message':"add stock"}
+
+@app.route('/stock/<int:id>/sell',methods=["POST"])
+def sell():
+    return{'message':"sell stock"}
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
--- models.py
+++ models.py
@@ -1,2 +1,52 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String,nullable=False,unique=True)
+    password = db.Column(db.String,nullable=False)
+    balance = db.Column(db.Integer, nullable=False)
+
+    def to_json(self):
+        return{
+            'id':self.id,
+            'name':self.name,
+            'email':self.email,
+            'balance':self.balance
+        }
+
+
+class Crypto(db.Model):
+    __tablename__ ='crypto'
+
+    id = db.Column(db.Integer,primary_key=True)
+    crypto_code = db.Column(db.String)
+
+    def to_json(self):
+        return{
+            'id': self.id,
+            "crypto_code" :self.crypto_code
+        }
+
+
+class User_crypto(db.Model):
+    __tablename__ = 'user_crypto'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+    amount = db.Column(db.Integer)
+    
+
+    def to_json(self):
+        return{
+            'id':self.id,
+            'user_id':self.user_id,
+            'user_id':self.user_id,
+            'amount':self.amount
+        }
+   
